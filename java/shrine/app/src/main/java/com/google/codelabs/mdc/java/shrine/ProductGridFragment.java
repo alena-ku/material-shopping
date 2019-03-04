b241a0abf6c266ac954006aee3a162daca3152da
alena <49859442+alena-dev@users.noreply.github.com>
2019-03-04 18:47:05 +0300

    Task-2. Add a top app bar (Toolbar)

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -2,17 +2,49 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 public class ProductGridFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        //Set up the toolbar
+        setUpToolbar(view);
+
+        return view;
+    }
+
+    private void setUpToolbar (View view){
+        android.support.v7.widget.Toolbar toolbar = view
+                .findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null){
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
